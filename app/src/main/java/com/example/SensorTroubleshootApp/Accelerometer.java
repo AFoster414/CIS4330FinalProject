d10761e4bf8b8be9737f26919184347b0583b580
Austin Foster <austinrfoster2001@gmail.com>
2022-10-23 11:46:16 -0400

    Renamed project, included every file necessary to load project in android studio, and added tabs so we can switch between different sensors that will be implemented later on

---
+++ app/src/main/java/com/example/SensorTroubleshootApp/Accelerometer.java
@@ -0,0 +1,132 @@
+package com.example.SensorTroubleshootApp;
+
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Accelerometer#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Accelerometer extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+
+    TextView txt_currentAccel, txt_prevAccel, txt_acceleration;
+    ProgressBar prog_shakeMeter;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private double accelerationCurrentValue;
+    private double accelerationPreviousValue;
+
+    public Accelerometer() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Accelerometer.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Accelerometer newInstance(String param1, String param2) {
+        Accelerometer fragment = new Accelerometer();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_accelerometer, container, false);
+        super.onViewCreated(v, savedInstanceState);
+        prog_shakeMeter = (ProgressBar) v.findViewById(R.id.prog_shakeMeter);
+        txt_acceleration = (TextView) v.findViewById(R.id.txt_accel);
+        txt_currentAccel = (TextView) v.findViewById(R.id.txt_currentAccel);
+        txt_prevAccel = (TextView) v.findViewById(R.id.txt_prevAccel);
+
+        //initialize sensor objects
+        mSensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        return v;
+    }
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            accelerationCurrentValue = Math.sqrt(x * x + y * y + z * z);
+            double changeInAcceleration = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
+            accelerationPreviousValue = accelerationCurrentValue;
+
+            //update text values
+            txt_currentAccel.setText("Current Acceleration = "+ (int) accelerationCurrentValue);
+            txt_prevAccel.setText("Previous Acceleration = " + (int) accelerationPreviousValue);
+            txt_acceleration.setText("Acceleration Change = " + (int) changeInAcceleration);
+
+            prog_shakeMeter.setProgress((int)changeInAcceleration);
+
+            if(changeInAcceleration > 14){
+                prog_shakeMeter.setBackgroundColor(Color.RED);
+            }else if(changeInAcceleration > 5){
+                prog_shakeMeter.setBackgroundColor(Color.parseColor("#edc540"));
+            }else if(changeInAcceleration > 2){
+                prog_shakeMeter.setBackgroundColor(Color.YELLOW);
+            }else{
+                prog_shakeMeter.setBackgroundColor(getResources().getColor(com.google.android.material.R.color.design_default_color_background));
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    }; //object that listens for changes in accelerometer, triggers when sensor is used
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(sensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(sensorEventListener);
+    }
+}
---
+++ app/src/main/java/com/example/SensorTroubleshootApp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.SensorTroubleshootApp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Accelerometer"));
+        tabLayout.addTab(tabLayout.newTab().setText("Other"));
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                switch(position) {
+                    case 0:
+                        Accelerometer accel = new Accelerometer();
+                        return accel;
+                    case 1:
+                        Temperature temp = new Temperature();
+                        return temp;
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return tabLayout.getTabCount();
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+
+
+}
